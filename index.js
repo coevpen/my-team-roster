2eb755d98a72aed06b087f618029b9096db4ef05
coevpen <koinuevins@gmail.com>
2022-01-15 15:15:27 -0600

    add fileWrite.js to src folder to move filewrite code out of index.js

--- index.js
+++ index.js
@@ -1,59 +1,10 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const fileWriting = require('./src/fileWrite');
 const generatePage = require('./src/page-template');
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 
-
-// copy css to dist folder
-const copyFile = () => {
-    return new Promise((resolve, reject) => {
-      fs.copyFile('./src/style.css', './dist/style.css', err => {
-        if (err) {
-          reject(err);
-          return;
-        }
-  
-        resolve({
-          ok: true,
-          message: 'Stylesheet created!'
-        });
-      });
-    });
-};
-
-//call to write the top of the html page index.html
-const writeFile = createHTML => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile('./dist/index.html', createHTML, err =>{
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve({
-        ok: true,
-        message: 'File created!'
-      });
-    }); 
-  });
-};
-
-//call to write the middle and bottom parts of the file
-const appendFile = bodyContent => {
-  return new Promise((resolve, reject) => {
-    fs.appendFile('./dist/index.html', bodyContent, err=> {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve({
-        ok: true,
-        message: 'File updated.'
-      });
-    });
-  });
-};
 
 // prompt the user for info
 const initManager = () => {
@@ -120,7 +71,7 @@ const initManager = () => {
       .then(managerInfo => {
         // create the index.html page with html before the employee cards
         const topPage = generatePage.generateTop();
-        writeFile(topPage);
+        fileWriting.writeFile(topPage);
 
         // creates the manager object with the info given
         const { name, id, email, officeNum} = managerInfo;
@@ -128,7 +79,7 @@ const initManager = () => {
 
         // create the manager card and append it to the html page
         const managerHTML = generatePage.addManager(manager);
-        appendFile(managerHTML);
+        fileWriting.appendFile(managerHTML);
       })
 };
 
@@ -226,13 +177,13 @@ const addEmployees = () => {
       if(role === "Engineer"){
         let engineer = new Engineer(name, id, email, github);
         let engineerHMTL = generatePage.addEngineer(engineer);
-        appendFile(engineerHMTL);
+        fileWriting.appendFile(engineerHMTL);
 
       }
       else if(role === "Intern"){
         let intern = new Intern(name, id, email, school);
         let internHTML = generatePage.addIntern(intern);
-        appendFile(internHTML);
+        fileWriting.appendFile(internHTML);
       }
       if(confirmAddEmployees){
         addEmployees();
@@ -243,5 +194,5 @@ const addEmployees = () => {
 // Function call to initialize app
 initManager()
   .then(addEmployees)
-  .then(appendFile(generatePage.generateBottom()))
-  .then(copyFile());
+  .then(fileWriting.appendFile(generatePage.generateBottom()))
+  .then(fileWriting.copyFile());
---
+++ src/fileWrite.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+
+// copy css to dist folder
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'Stylesheet created!'
+        });
+      });
+    });
+};
+
+//call to write the top of the html page index.html
+const writeFile = createHTML => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', createHTML, err =>{
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    }); 
+  });
+};
+
+//call to write the middle and bottom parts of the file
+const appendFile = bodyContent => {
+  return new Promise((resolve, reject) => {
+    fs.appendFile('./dist/index.html', bodyContent, err=> {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve({
+        ok: true,
+        message: 'File updated.'
+      });
+    });
+  });
+};
+
+module.exports = {
+    copyFile,
+    writeFile,
+    appendFile
+};
